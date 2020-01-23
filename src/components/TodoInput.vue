638a782b1d051931cdfea108e82c78a5dde91f94
trueminds <park.taebin@gmail.com>
2020-01-23 11:42:56 +0900

    updated buttons

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
-    <input type="text" v-model="newTodoItem" />
-    <button v-on:click="addTodo">add</button>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+    <!-- <button v-on:click="addTodo">add</button> -->
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="fas fa-plus addBtn"></i>
+    </span>
   </div>
 </template>
 
@@ -16,10 +19,42 @@ export default {
     methods:  {
       addTodo: function() {
          // eslint-disable-next-line no-console
-         console.log(this.newTodoItem);
+        //  console.log(this.newTodoItem);
+         //저장하는 로직
+         localStorage.setItem(this.newTodoItem,this.newTodoItem);
+         this.clearInput();
+      },
+      clearInput: function() {
+         this.newTodoItem = ''
       }
     }
 };
 </script>
 
-<style></style>
+<style scoped>
+input:focus {
+  outline: none
+}
+.inputBox {
+  background : white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+
+</style>
