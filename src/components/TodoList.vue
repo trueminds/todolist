60457ad49bc5b57ce70fdea83ad07536b7f777cc
TB Park <park.taebin@gmail.com>
2020-01-24 16:23:27 +0900

    updated
    updated from local

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
     <ul>
-      <li v-for="todoItem in todoItems" v-bind:key = "todoItem"></li>
-      <li>박하원짱</li>
- </ul>
+      <li v-for="(todoItem,index) in todoItems" v-bind:key = "todoItem" class="shadow">
+      {{todoItem}}
+      <span class="removeBtn" v-on:click = "removeTodo(todoItem,index)">
+        <i class="fas fa-trash-alt"></i>
+      </span>
+      <!-- <button vo-on:click="removeTodo">delete</button> -->
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -11,7 +16,15 @@
 export default {
   data:function(){
     return{
-      todoItems: []  
+      todoItems: []            
+    }
+  },  
+  methods: {
+    removeTodo: function(todoItem,index){
+      // eslint-disable-next-line no-console
+      console.log(todoItem,index);
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index,1);
     }
   },
   created: function(){
@@ -28,4 +41,36 @@ export default {
 </script>
 
 <style>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  color: #b3adad;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
 </style>
